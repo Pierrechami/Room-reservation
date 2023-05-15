a56a11a555089a10ae00724e2239afb9764ad0ca
Pablog <pablo.foucher1@gmail.com>
2023-05-15 08:15:45 +0200

    init

---
+++ SalleReserver/src/main/java/com/epsi/SalleReserver/SalleReserverApplication.java
@@ -0,0 +1,13 @@
+package com.epsi.SalleReserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalleReserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalleReserverApplication.class, args);
+	}
+
+}
