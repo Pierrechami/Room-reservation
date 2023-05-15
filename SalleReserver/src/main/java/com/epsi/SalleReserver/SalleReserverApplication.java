4e263ff2f25d75631bbddfad588834f400e872de
Pablog <pablo.foucher1@gmail.com>
2023-05-15 09:25:48 +0200

    fixtures

--- SalleReserver/src/main/java/com/epsi/SalleReserver/SalleReserverApplication.java
+++ SalleReserver/src/main/java/com/epsi/SalleReserver/SalleReserverApplication.java
@@ -1,13 +1,24 @@
 package com.epsi.SalleReserver;
 
+import com.epsi.SalleReserver.Repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SalleReserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SalleReserverApplication.class, args);
-	}
+}
+	@Autowired
+	UserRepository userRepository;
 
+	public static void main(String[] args) {SpringApplication.run(SalleReserverApplication.class, args);}
+
+	@PostConstruct
+	public void init() {
+
+
+	}
 }
